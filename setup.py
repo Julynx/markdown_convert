e9cc4f942ace714b22913e8c2c6eb9b786ec9f1c
Julio <julicabria@outlook.com>
2023-10-22 19:03:30 +0200

    0.9.1 release

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('README.md', 'r', encoding='utf-8') as markdown_file:
 
 setuptools.setup(
     name='markdown_convert',
-    version='0.9.0',
+    version='0.9.1',
     author='Julio Cabria',
     author_email='juliocabria@tutanota.com',
     maintainer='Julio Cabria',
     maintainer_email='juliocabria@tutanota.com',
     url='https://github.com/Julynx/markdown_convert',
-    description='Extract one or multiple substrings between two start and end strings.',
+    description='Python package to convert Markdown files to other formats.',
     long_description=readme_contents,
     long_description_content_type='text/markdown',
     license='GPLv2',
