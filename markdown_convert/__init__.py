b892128c9fd285365213282aaa98f87de15edd61
Julio <julicabria@outlook.com>
2024-04-08 23:29:03 +0200

    Linting

--- markdown_convert/__init__.py
+++ markdown_convert/__init__.py
@@ -1,2 +1,7 @@
+"""
+This file is used to import all the functions from the modules and make them
+available to the user.
+"""
+
 from __main__ import *
 from .modules.convert import convert, live_convert
